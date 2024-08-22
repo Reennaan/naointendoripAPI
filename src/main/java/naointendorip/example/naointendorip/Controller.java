266397fa5af685ff0583dc96ae171c9dfe648632
Renan <qwerty142999@gmail.com>
2024-08-22 19:41:38 -0300

    adding-search-game-method

--- src/main/java/naointendorip/example/naointendorip/Controller.java
+++ src/main/java/naointendorip/example/naointendorip/Controller.java
@@ -3,6 +3,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,16 +16,23 @@ import java.util.List;
 public class Controller {
     String url = "https://switchromz.com";
 
+
+    public Elements getDocument(String selector, String url) throws IOException {
+        Document doc = Jsoup.connect(url).timeout(90000).get();
+        return doc.select(selector);
+    }
+
+
     @GetMapping("/game")
-    public List<Game> recentgames() throws IOException {
+    public List<Game> recentGames() throws IOException {
+        Document doc = Jsoup.connect(url).timeout(90000).get();
+
 
         List<Game>result = new ArrayList<>();
-        Document doc = Jsoup.connect(url).timeout(90000).get();
+
         Elements table = doc.select("#post-273 > div > div > div.wp-block-kadence-column.kadence-column273_7d54f2-a4 > div > div > div");
         Elements imgs = table.select("img");
         Elements links = table.select("a");
-
-
 
         System.out.println(links);
 
@@ -40,6 +49,28 @@ public class Controller {
         }
 
         return result;
+    }
+
+    @GetMapping("/search/{name}")
+    @ResponseBody
+    public List<Game> search(@PathVariable("name") String name) throws IOException {
+        Document doc = Jsoup.connect(url+"?s="+name).timeout(90000).get();
+        List<Game> games = new ArrayList<>();
+
+
+        Elements links = doc.select("article.entry a.post-thumbnail");
+        Elements imgs = doc.select("article.entry a.post-thumbnail div.post-thumbnail-inner img");
+        System.out.println(links);
+        for(int i = 0; i < links.size(); i++ ){
+            Game game = new Game();
+            game.setId(i);
+            game.setLink(links.get(i).attr("href"));
+            game.setName(imgs.get(i).attr("alt"));
+            game.setImg(imgs.get(i).attr("srcset"));
+            games.add(game);
+        }
+
+        return games;
     }
 
 
