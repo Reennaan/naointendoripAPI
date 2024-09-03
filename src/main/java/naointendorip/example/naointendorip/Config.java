08503481cc5a918cd9ac3618036dbb3fe416b5d6
Renan <qwerty142999@gmail.com>
2024-09-03 17:25:29 -0300

    trying-tohard

--- src/main/java/naointendorip/example/naointendorip/Config.java
+++ src/main/java/naointendorip/example/naointendorip/Config.java
@@ -10,7 +10,7 @@ public class Config implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("https://renanandrade000.github.io/Naointendorip/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
