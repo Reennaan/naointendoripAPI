ffc26dfd161e45706630b2a360e7700202a0d8ba
Renan <qwerty142999@gmail.com>
2024-09-03 17:04:09 -0300

    adding-permissions

---
+++ src/main/java/naointendorip/example/naointendorip/PermissionsPolicyFilter.java
@@ -0,0 +1,33 @@
+package naointendorip.example.naointendorip;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+public class PermissionsPolicyFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Permissions-Policy", "interest-cohort=()");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // init config if needed
+    }
+
+    @Override
+    public void destroy() {
+        // clean up if needed
+    }
+}
