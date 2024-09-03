abdaa52661411a027ef00f5c6a93cb655a514063
Renan <qwerty142999@gmail.com>
2024-09-03 17:32:16 -0300

    again

--- src/main/java/naointendorip/example/naointendorip/PermissionsPolicyFilter.java
+++ src/main/java/naointendorip/example/naointendorip/PermissionsPolicyFilter.java
@@ -16,8 +16,8 @@ public class PermissionsPolicyFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Permissions-Policy", "geolocation=(self), microphone=()");
+        //HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        //httpServletResponse.setHeader("Permissions-Policy", "geolocation=(self), microphone=()");
         chain.doFilter(request, response);
     }
 
